fe4d12a46de5b704d3d65a471ad6f03a1d5e2ec2
alexsoleleris <alexsoleleris@gmail.com>
2018-10-01 13:10:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board=[
+def display_board(board)
+
+board = [
   [" ", " ", " "],
   [" ", " ", " "],
   [" ", " ", " "]
 ]
-end
+
 
 
